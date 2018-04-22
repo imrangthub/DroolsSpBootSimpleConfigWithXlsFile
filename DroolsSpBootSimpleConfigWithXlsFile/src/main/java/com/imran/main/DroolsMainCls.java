9d5588992107a1644f848cef67f1f6b420ffb521
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2018-04-22 15:40:52 +0600

    add project at a time

---
+++ DroolsSpBootSimpleConfigWithXlsFile/src/main/java/com/imran/main/DroolsMainCls.java
@@ -0,0 +1,33 @@
+package com.imran.main;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+import com.imran.model.Product;
+
+public class DroolsMainCls {
+
+	public static final void main(String[] args) {
+		try {
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+			KieSession kSession = kContainer.newKieSession("ksession-rule");
+
+			Product product = new Product();
+			product.setType("other");
+
+			FactHandle fact1;
+
+			fact1 = kSession.insert(product);
+			kSession.fireAllRules();
+
+			System.out.println("The discount for the jewellery product "+product.getType() + " is " + product.getDiscount());
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+}
